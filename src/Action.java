63a8282a512f7cf8eca6b51b33a7bf6d18f4717f
shaha <shahafwieder@campus.technion.ac.il>
2023-05-13 18:16:40 +0300

    Changes by sagid

--- src/Action.java
+++ src/Action.java
@@ -3,12 +3,12 @@ private  Board board;
 private Tile tile;
 private EnumDirection direction;
 public Action(Tile t,Board b,EnumDirection ed){
-    this.tile=t;
-    this.board=b;
-    this.direction=ed;
+   this.tile=t;
+   this.board=b;
+   this.direction=ed;
 }
 public String toString(){
-    String action = "Move" + tile + direction;
+    String action = "Move" + tile.getValue() + direction;
     return action;
     }
 }
